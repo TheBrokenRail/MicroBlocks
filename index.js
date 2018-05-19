b556be59d51bb33f9b95ab744a8b909c413b9cd2
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-19 15:48:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+window.ScratchBlocks = require('scratch-blocks');
+window.html_beautify = require('js-beautify').html_beautify;
+window.xml_js = require("xml-js");
+require('scratch-blocks/javascript_compressed_nodejs')(window.ScratchBlocks);
