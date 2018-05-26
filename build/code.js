959dca5b095dd802b6b523c3a12c718822e9e8ac
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-26 12:40:41 -0400

    Update code.js

--- build/code.js
+++ build/code.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     let workspace = window.ScratchBlocks.inject('blocks', {
         media: './media/',
-        toolbox: document.getElementById('toolbox'),
+        toolbox: null,
         zoom: {
             controls: true,
             wheel: true,
@@ -13,7 +13,7 @@ window.onload = function () {
         }
     });
     workspace.registerToolboxCategoryCallback('METHODS', methodsCallback);
-    
+    workspace.updateToolbox(document.getElementById('toolbox'));
     window.workspace = workspace;
     let css = window.ScratchBlocks.Css.styleSheet_.cssRules;
     for (i = 0; i < css.length; i++) {
