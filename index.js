9aef6678adc6e008839af437d4c36d9a750c3202
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-26 15:39:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const ncp = require('ncp').ncp;
 const express = require('express');
 const app = express();
 
-let file = fs.readFileSync('./node_modules/scratch-blocks/javascript_compressed.js', {encoding: "utf8"});
-file = 'module.exports = function(Blockly){' + file + '};';
-fs.writeFileSync('./node_modules/scratch-blocks/javascript_compressed_nodejs.js', file);
 browserify.add('build.js');
-
 app.use('/editor', express.static('build'));
 browserify.bundle((err, data) => {
   if (err) {
     throw err;
   }
   fs.writeFileSync('build/bundle.js', data);
-  ncp('node_modules/scratch-blocks/media', 'build/media', ncpErr => {
+  ncp('node_modules/blockly/media', 'build/media', ncpErr => {
     if (ncpErr) {
       throw ncpErr;
     }
