f1029229c7186b5bb6409cc0297cc8cdf2f4ad52
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-26 15:36:28 -0400

    Update build.js

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-window.ScratchBlocks = require('blockly');
+window.ScratchBlocks = require('./blockly');
 window.html_beautify = require('js-beautify').html_beautify;
 window.xml_js = require("xml-js");
 require('scratch-blocks/javascript_compressed_nodejs')(window.ScratchBlocks);
