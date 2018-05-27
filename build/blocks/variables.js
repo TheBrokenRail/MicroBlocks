b0c613160439f27888b656174d729a4c4369a381
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-27 17:05:38 -0400

    Create variables.js

---
+++ build/blocks/variables.js
@@ -0,0 +1,20 @@
+Blockly.Blocks['&&variables_get'] = {
+  init: function () {
+    this.jsonInit({
+      type: '&&variables_get',
+      message0: 'get %1',
+      args0: [
+        {
+          type: 'field_dropdown',
+          name: 'NAME',
+          options: function () {
+            return [];
+          }
+        }
+      ],
+      inputsInline: true,
+      output: 'MISSING_TYPE',
+      colour: 160
+    });
+  }
+};
