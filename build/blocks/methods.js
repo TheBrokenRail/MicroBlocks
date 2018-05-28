3c1241c7a228e635f4c430f5753c6182aef925be
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-28 11:18:04 -0400

    Update methods.js

--- build/blocks/methods.js
+++ build/blocks/methods.js
@@ -17,7 +17,7 @@ Blockly.Blocks['&&methods_def'] = {
     this.setColour(210);
     this.arguments_ = [];
     this.appendStatementInput('STACK');
-    this.statementConnection_ = null;
+    this.bad_ = false;
   },
   /**
    * Update the display of parameters for this procedure definition block.
@@ -27,19 +27,29 @@ Blockly.Blocks['&&methods_def'] = {
    */
   updateParams_: function () {
     // Check for duplicated arguments.
-    let badArg = false;
+    let badArg = [];
     let hash = {};
     for (let i = 0; i < this.arguments_.length; i++) {
-      if (hash['arg_' + this.arguments_[i].name] || this.arguments_[i].name.indexOf(' ') !== -1) {
-        badArg = true;
+      if (hash['arg_' + this.arguments_[i].name]) {
+        badArg.push('Duplicated "' + this.arguments_[i].name + '" Arguments!');
+      }
+      if (this.arguments_[i].name.indexOf(' ') !== -1) {
+        badArg.push('Argument "' + this.arguments_[i].name + '" Has Spaces!');
+      }
+      if (this.arguments_[i].name.length < 1) {
+        badArg.push('Argument ' + (i + 1) + ' Has No Name!');
+      }
+      if (badArg.length > 0) {
         break;
       }
       hash['arg_' + this.arguments_[i].name] = true;
     }
     if (badArg) {
-      this.setWarningText('Bad!');
+      this.setWarningText(badArg.join('\n'));
+      this.bad_ = true;
     } else {
       this.setWarningText(null);
+      this.bad_ = false;
     }
     // Merge the arguments into a human-readable list.
     let paramString = '';
@@ -148,7 +158,7 @@ Blockly.Blocks['&&methods_def'] = {
    * @this Blockly.Block
    */
   getProcedureDef: function () {
-    return [(this.getInputTargetBlock('TYPE') ? this.getInputTargetBlock('TYPE').type : 'MISSING_TYPE'), this.getFieldValue('NAME'), false, this.arguments_];
+    return [(this.getInputTargetBlock('TYPE') ? this.getInputTargetBlock('TYPE').type : 'MISSING_TYPE'), this.getFieldValue('NAME'), false, this.arguments_, this.bad_];
   },
   /**
    * Return all letiables referenced by this block.
@@ -204,10 +214,6 @@ Blockly.Blocks['&&methods_mutatorarg'] = {
 };
 
 Blockly.Blocks['&&methods_call'] = {
-  /**
-   * Block for calling a procedure with no return value.
-   * @this Blockly.Block
-   */
   init: function () {
     this.appendDummyInput('TOPROW')
         .appendField(this.id, 'NAME');
@@ -216,8 +222,12 @@ Blockly.Blocks['&&methods_call'] = {
     this.setColour(210);
     this.arguments_ = [];
     this.type_ = '';
+    this.output_ = false;
   },
   setArguments: function (type, name) {
+    if (this.output_) {
+      this.setOutput(type);
+    }
     let list = Blockly.Procedures.allProcedures(workspace)[0];
     for (let i = 0; i < list.length; i++) {
       if (list[i][0] === type && list[i][1] === name) {
@@ -225,50 +235,17 @@ Blockly.Blocks['&&methods_call'] = {
       }
     }
   },
-  /**
-   * Returns the name of the procedure this block calls.
-   * @return {string} Procedure name.
-   * @this Blockly.Block
-   */
   getProcedureCall: function () {
-    // The NAME field is guaranteed to exist, null will never be returned.
     return [this.getFieldValue('NAME'), this.type_];
   },
-  /**
-   * Notification that a procedure is renaming.
-   * If the name matches this block's procedure, rename it.
-   * @param {string} oldName Previous name of procedure.
-   * @param {string} newName Renamed procedure.
-   * @this Blockly.Block
-   */
   renameProcedure: function (oldName, newName) {
     if (Blockly.Names.equals(oldName, this.getProcedureCall()[0])) {
       this.setFieldValue(newName, 'NAME');
     }
   },
-  /**
-   * Notification that the procedure's parameters have changed.
-   * @param {!Array.<string>} paramNames New param names, e.g. ['x', 'y', 'z'].
-   * @param {!Array.<string>} paramIds IDs of params (consistent for each
-   *     parameter through the life of a mutator, regardless of param renaming),
-   *     e.g. ['piua', 'f8b_', 'oi.o'].
-   * @private
-   * @this Blockly.Block
-   */
   setProcedureParameters_: function () {
-    // Data structures:
-    // this.arguments = ['x', 'y']
-    //     Existing param names.
-    // this.quarkConnections_ {piua: null, f8b_: Blockly.Connection}
-    //     Look-up of paramIds to connections plugged into the call block.
-    // this.quarkIds_ = ['piua', 'f8b_']
-    //     Existing param IDs.
-    // Note that quarkConnections_ may include IDs that no longer exist, but
-    // which might reappear if a param is reattached in the mutator.
-    // Switch off rendering while the block is rebuilt.
     let savedRendered = this.rendered;
     this.rendered = false;
-    // Update the quarkConnections_ with existing connections.
     let quark = {};
     for (let i = 0; i < this.arguments_.length; i++) {
       let input = this.getInput('ARG' + i);
@@ -276,44 +253,31 @@ Blockly.Blocks['&&methods_call'] = {
         let connection = input.connection.targetConnection;
         quark[i] = connection;
         if (connection) {
-          // This connection should no longer be attached to this block.
           connection.disconnect();
           connection.getSourceBlock().bumpNeighbours_();
         }
       }
     }
-    // Rebuild the block's arguments.
     this.updateShape_();
-    // Reconnect any child blocks.
     for (let i = 0; i < this.arguments_.length; i++) {
       if (quark[i]) {
         let connection = quark[i];
         if (!Blockly.Mutator.reconnect(connection, this, 'ARG' + i)) {
-          // Block no longer exists or has been attached elsewhere.
           delete this.quarkConnections_[quarkId];
         }
       }
     }
-    // Restore rendering and show the changes.
     this.rendered = savedRendered;
     if (this.rendered) {
       this.render();
     }
   },
-  /**
-   * Modify this block to have the correct number of arguments.
-   * @private
-   * @this Blockly.Block
-   */
   updateShape_: function () {
     this.setArguments(this.getProcedureCall()[1], this.getProcedureCall()[0]);
     let i = null;
     for (i = 0; i < this.arguments_.length; i++) {
       let field = this.getField('ARGNAME' + i);
       if (field) {
-        // Ensure argument name is up to date.
-        // The argument name field is deterministic based on the mutation,
-        // no need to fire a change event.
         Blockly.Events.disable();
         try {
           field.setValue(this.arguments_[i].name + ':');
@@ -321,7 +285,6 @@ Blockly.Blocks['&&methods_call'] = {
           Blockly.Events.enable();
         }
       } else {
-        // Add new input.
         field = new Blockly.FieldLabel(this.arguments_[i].name);
         let input = this.appendValueInput('ARG' + i)
             .setAlign(Blockly.ALIGN_RIGHT)
@@ -330,43 +293,25 @@ Blockly.Blocks['&&methods_call'] = {
         input.init();
       }
     }
-    // Remove deleted inputs.
     while (this.getInput('ARG' + i)) {
       this.removeInput('ARG' + i);
       i++;
     }
   },
-  /**
-   * Create XML to represent the (non-editable) name and arguments.
-   * @return {!Element} XML storage element.
-   * @this Blockly.Block
-   */
   mutationToDom: function () {
     let container = document.createElement('mutation');
     container.setAttribute('name', this.getProcedureCall()[0]);
     container.setAttribute('type', this.getProcedureCall()[1]);
     return container;
   },
-  /**
-   * Parse XML to restore the (non-editable) name and parameters.
-   * @param {!Element} xmlElement XML storage element.
-   * @this Blockly.Block
-   */
   domToMutation: function (xmlElement) {
     let name = xmlElement.getAttribute('name');
     this.type_ = xmlElement.getAttribute('type');
     this.renameProcedure(this.getProcedureCall()[0], name);
     this.setProcedureParameters_();
   },
-  /**
-   * Procedure calls cannot exist without the corresponding procedure
-   * definition.  Enforce this link whenever an event is fired.
-   * @param {!Blockly.Events.Abstract} event Change event.
-   * @this Blockly.Block
-   */
   onchange: function (event) {
     if (!this.workspace || this.workspace.isFlyout) {
-      // Block is deleted or is in a flyout.
       return;
     }
   },
@@ -381,11 +326,37 @@ Blockly.JavaScript['&&methods_call'] = function (block) {
   return block.getProcedureCall()[0] + '(' + args.join(',') + ');';
 };
 
+
+Blockly.Blocks['&&methods_call_output'] = {
+  init: function () {
+    this.appendDummyInput('TOPROW')
+        .appendField(this.id, 'NAME');
+    this.setOutput('MISSING_TYPE');
+    this.setColour(210);
+    this.arguments_ = [];
+    this.type_ = '';
+    this.output_ = true;
+  },
+  setArguments: Blockly.Blocks['&&methods_call'].setArguments,
+  getProcedureCall: Blockly.Blocks['&&methods_call'].getProcedureCall,
+  renameProcedure: Blockly.Blocks['&&methods_call'].renameProcedure,
+  setProcedureParameters_: Blockly.Blocks['&&methods_call'].setProcedureParameters_,
+  updateShape_: Blockly.Blocks['&&methods_call'].updateShape_,
+  mutationToDom: Blockly.Blocks['&&methods_call'].mutationToDom,
+  domToMutation: Blockly.Blocks['&&methods_call'].domToMutation,
+  onchange: Blockly.Blocks['&&methods_call'].onchange,
+  defType_: '&&methods_def'
+};
+
+Blockly.JavaScript['&&methods_call_output'] = function (block) {
+  let args = [];
+  for (let i = 0; i < block.arguments_.length; i++) {
+    args.push(Blockly.JavaScript.valueToCode(block, 'ARG' + i));
+  }
+  return block.getProcedureCall()[0] + '(' + args.join(',') + ');';
+};
+
 Blockly.Blocks['&&methods_return'] = {
-  /**
-   * Block for conditionally returning a value from a procedure.
-   * @this Blockly.Block
-   */
   init: function () {
     this.appendValueInput('VALUE')
         .appendField('return');
@@ -403,39 +374,29 @@ Blockly.JavaScript['&&methods_return'] = function (block) {
 const methodsCallback = workspace => {
   let xmlList = [];
   if (Blockly.Blocks['&&methods_def']) {
-    // <block type="procedures_defnoreturn" gap="16">
-    //     <field name="NAME">do something</field>
-    // </block>
     let block = document.createElement('block');
     block.setAttribute('type', '&&methods_def');
-    block.setAttribute('gap', 16);
     let nameField = document.createElement('field');
     nameField.setAttribute('NAME', '');
     block.appendChild(nameField);
     xmlList.push(block);
   }
   if (Blockly.Blocks['&&methods_return']) {
-    // <block type="procedures_ifreturn" gap="16"></block>
     var block = document.createElement('block');
     block.setAttribute('type', '&&methods_return');
-    block.setAttribute('gap', 16);
     xmlList.push(block);
   }
   if (xmlList.length) {
-    // Add slightly larger gap between system blocks and user calls.
     xmlList[xmlList.length - 1].setAttribute('gap', 24);
   }
-
   function populateProcedures(procedureList, templateName) {
-    for (var i = 0; i < procedureList.length; i++) {
+    for (let i = 0; i < procedureList.length; i++) {
+      if (procedureList[i][4]) {
+        continue;
+      }
       let type = procedureList[i][0];
       let name = procedureList[i][1];
       let args = procedureList[i][3];
-      // <block type="procedures_callnoreturn" gap="16">
-      //   <mutation name="do something">
-      //     <arg name="x"></arg>
-      //   </mutation>
-      // </block>
       let block = document.createElement('block');
       block.setAttribute('type', templateName);
       block.setAttribute('gap', 16);
@@ -444,6 +405,14 @@ const methodsCallback = workspace => {
       mutation.setAttribute('type', type);
       block.appendChild(mutation);
       xmlList.push(block);
+      let blockOutput = document.createElement('block');
+      blockOutput.setAttribute('type', templateName + '_output');
+      blockOutput.setAttribute('gap', 16);
+      let mutationOutput = document.createElement('mutation');
+      mutationOutput.setAttribute('name', name);
+      mutationOutput.setAttribute('type', type);
+      blockOutput.appendChild(mutationOutput);
+      xmlList.push(blockOutput);
     }
   }
   let tuple = Blockly.Procedures.allProcedures(workspace);
