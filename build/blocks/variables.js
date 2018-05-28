e69139c7550fe96be7cb3673751c9f33b5ef6316
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-28 10:53:52 -0400

    Update variables.js

--- build/blocks/variables.js
+++ build/blocks/variables.js
@@ -89,7 +89,7 @@ Blockly.Blocks['&&variables_set'] = {
 Blockly.JavaScript['&&methods_set'] = function (block) {
   return block.getFieldValue('VARIABLE') + ' = ' + Blockly.JavaScript.valueToCode(block, 'VALUE') + ';';
 };
-Blockly.Blocks['&&variables_initialize'] = {
+Blockly.Blocks['&&variables_initialize_to'] = {
   init: function () {
     this.appendValueInput('TYPE')
         .setCheck('C++Type')
@@ -97,6 +97,27 @@ Blockly.Blocks['&&variables_initialize'] = {
     this.appendValueInput('VALUE')
         .appendField(new Blockly.FieldTextInput(''), 'NAME')
         .appendField('to');
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(20);
+  },
+  onchange: function () {
+    this.getInput('VALUE').setCheck(this.getInputTargetBlock('TYPE') ? this.getInputTargetBlock('TYPE').type: 'MISSING_TYPE');
+  },
+  getVar: function () {
+    return [{name: this.getFieldValue('NAME'), type: this.getInputTargetBlock('TYPE')}];
+  }
+};
+Blockly.JavaScript['&&variables_initialize_to'] = function (block) {
+  return block.getInputTargetBlock('TYPE').type + ' ' + block.getFieldValue('NAME') + ' = ' + Blockly.JavaScript.valueToCode(block, 'VALUE') + ';';
+};
+Blockly.Blocks['&&variables_initialize'] = {
+  init: function () {
+    this.appendValueInput('TYPE')
+        .setCheck('C++Type')
+        .appendField('initialize');
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput(''), 'NAME')
     this.setInputsInline(true);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
@@ -110,5 +131,5 @@ Blockly.Blocks['&&variables_initialize'] = {
   }
 };
 Blockly.JavaScript['&&variables_initialize'] = function (block) {
-  return block.getInputTargetBlock('TYPE').type + ' ' + block.getFieldValue('NAME') + ' = ' + Blockly.JavaScript.valueToCode(block, 'VALUE') + ';';
+  return block.getInputTargetBlock('TYPE').type + ' ' + block.getFieldValue('NAME') + ';';
 };
