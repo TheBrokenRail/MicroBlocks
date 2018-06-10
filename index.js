5dbbbc0a37f42bd277dfad11e83131eda8ef0f69
TheBrokenRail <connor24nolan@live.com>
2018-06-10 11:36:02 -0400

    Work

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
 const fs = require('fs');
-const webpack = require('webpack')(require('./webpack.config.js'));
+const browserify = require('browserify')();
 const ncp = require('ncp').ncp;
 const express = require('express');
-const rimraf = require('rimraf');
 const app = express();
+const PORT = process.env.PORT || 80;
 
-app.use('/editor', express.static('dist'));
-app.use('/extensions', express.static('extensions'));
-let firstTime = true;
-if (fs.existsSync('dist/')) {
-  rimraf.sync('dist');
-}
-fs.mkdirSync('dist');
-fs.writeFileSync('dist/index.html', fs.readFileSync('src/index.html'));
-webpack.watch({}, (err, stats) => {
+browserify.add('build.js');
+app.use('/editor', express.static('build'));
+browserify.bundle((err, data) => {
   if (err) {
     throw err;
   }
-  console.log(stats.toString({
-    chunks: false,
-    colors: true
-  }));
-  if (firstTime) {
-    ncp('node_modules/blockly/media', 'dist/media', ncpErr => {
-      if (ncpErr) {
-        throw ncpErr;
-      }
-      app.listen(80, () => console.log('MicroBlocks listening on port 80'));
-    });
-    firstTime = false;
-  }
+  fs.writeFileSync('build/bundle.js', data);
+  ncp('node_modules/blockly/media', 'build/media', ncpErr => {
+    if (ncpErr) {
+      throw ncpErr;
+    }
+    app.listen(PORT, () => console.log('MicroBlocks listening on port 80'));
+  });
 });
